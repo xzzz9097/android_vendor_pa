4ff266a0ed2355c7f5d8f95538520889384ad157
Bryan Smith <bryan.smith.dev@gmail.com>
2015-01-06 05:55:13 -0500

    dlx: update build fingerprint
    And remove build ID that was extraneous.
    
    Change-Id: I4b9066155490c65b90ec614f5002ca0c2c46343c

--- products/pa_dlx.mk
+++ products/pa_dlx.mk
@@ -41,8 +41,7 @@ PRODUCT_MODEL := HTC6435LVW
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dlx \
-    BUILD_ID=JDQ39 \
-    BUILD_FINGERPRINT=htc/verizon_wwe/dlx:4.2.2/JDQ39/277065.4:user/release-keys \
-    PRIVATE_BUILD_DESC="3.06.605.4 CL277065 release-keys"
+    BUILD_FINGERPRINT=htc/verizon_wwe/dlx:4.4.2/KOT49H/322791.5:user/release-keys \
+    PRIVATE_BUILD_DESC="4.09.605.5 CL322791 release-keys"
 
 endif
