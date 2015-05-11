246dbe8932a221f0b6d12ae91aba90ca1a653052
TheStrix <parthbhatia98@gmail.com>
2015-05-11 03:38:56 -0400

    condor: AOSPA-L bringup
    Change-Id: I9fda42126935a0d9f0242499db25eebd1ca75879
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -30,7 +30,7 @@ ifneq ($(filter pa_nozomi pa_falcon pa_tomato pa_armani pa_evita pa_xt926 moto_m
 endif
 
 # PA HDPI Devices
-ifneq ($(filter pa_p3100 pa_p3110 pa_taoshan,$(TARGET_PRODUCT)),)
+ifneq ($(filter pa_condor pa_p3100 pa_p3110 pa_taoshan,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pa/prebuilt/bootanimation/800x480.zip:system/media/bootanimation.zip
 endif
